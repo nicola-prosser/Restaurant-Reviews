22627da723c5a4e9c601f1100b3ca63485a842b3
Nicola-Prosser <nicola.prosser@outlook.com>
2018-07-09 21:50:23 +0100

    service worker updates

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('restaurantrevs-static-v1').then(function(cache) {
+        caches.open('static-v1').then(function(cache) {
             return cache.addAll([
-              '/',
-              './index.html',
-              './restaurant.html',
-              './css/styles.css',
-              './data/restaurants.json',
-              './img/1.jpg',
-              './img/2.jpg',
-              './img/3.jpg',
-              './img/4.jpg',
-              './img/5.jpg',
-              './img/6.jpg',
-              './img/7.jpg',
-              './img/8.jpg',
-              './img/9.jpg',
-              './img/10.jpg',
-              './js/main.js',
-              './js/restaurant_info.js',
-              './js/dbhelper.js',
+              './',
+                    './index.html',
+                    './restaurant.html',
+                    './css/styles.css',
+                    './js/main.js',
+                    './js/restaurant_info.js',
+                    './js/dbhelper.js',
+                    './img/1.jpg',
+                    './img/2.jpg',
+                    './img/3.jpg',
+                    './img/4.jpg',
+                    './img/5.jpg',
+                    './img/6.jpg',
+                    './img/7.jpg',
+                    './img/8.jpg',
+                    './img/9.jpg',
+                    './img/10.jpg',
             ]);
         })
     );
@@ -27,9 +26,13 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match('restaurantrevs-static-v1').then(function(response) {
-      if(response) return response;
-      return fetch(event.request);
-    })
-  );
-});
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function (response) {
+          return caches.open('static-v1').then(function (cache) {
+              cache.put(event.request, response.clone());
+              return response;
+          });
+        });
+      })
+    );
+  });
