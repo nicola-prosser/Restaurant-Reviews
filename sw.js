e8c562ba7238431a4982e28d7cbf469511c4ef24
Nicola-Prosser <nicola.prosser@outlook.com>
2018-07-10 19:26:04 +0100

    correcting SW.js file

--- sw.js
+++ sw.js
@@ -1,38 +1,37 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('static-v1').then(function(cache) {
-            return cache.addAll([
-              './',
-                    './index.html',
-                    './restaurant.html',
-                    './css/styles.css',
-                    './js/main.js',
-                    './js/restaurant_info.js',
-                    './js/dbhelper.js',
-                    './img/1.jpg',
-                    './img/2.jpg',
-                    './img/3.jpg',
-                    './img/4.jpg',
-                    './img/5.jpg',
-                    './img/6.jpg',
-                    './img/7.jpg',
-                    './img/8.jpg',
-                    './img/9.jpg',
-                    './img/10.jpg',
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('static-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './restaurant.html',
+        './css/styles.css',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './js/dbhelper.js',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function (response) {
-          return caches.open('static-v1').then(function (cache) {
-              cache.put(event.request, response.clone());
-              return response;
-          });
-        });
-      })
-    );
-  });
+self.addEventListener('fetch',function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    return response || fetch(event.request).then(function (response) {
+      return caches.open('static-v1').then(function (cache) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  })
+);
+});
